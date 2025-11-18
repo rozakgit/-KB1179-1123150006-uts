a3fc894b4d5774d89a7f1cd2fc11d18e95f2f2cd
rozakgit <muhammadabdulrozak21@gmail.com>
2025-11-18 22:37:35 +0700

    Create scaffold, center alignment

--- lib/screen/splash_screen_1.dart
+++ lib/screen/splash_screen_1.dart
@@ -5,6 +5,13 @@ class SplashScreen1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [],
+        ),
+      ),
+    );
   }
 }
