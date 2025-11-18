766e0355fe33bf71c396b8793295fea17caf8576
rozakgit <muhammadabdulrozak21@gmail.com>
2025-11-18 22:17:06 +0700

    False debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -6,3 +6,9 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false);
+  }
+}
